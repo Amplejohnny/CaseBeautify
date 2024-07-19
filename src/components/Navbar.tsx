04f2f5997adff06f8c2748c122c17dfc85773c30
Amplejohnny <uzunuel@gmail.com>
2024-07-19 15:26:41 +0100

    Annimation completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,11 +14,14 @@ const Navbar = async () => {
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link href="/" className="flex z-40 font-bold text-xl">
+          <Link
+            href="/"
+            className="flex z-40 font-semibold md:font-bold lg:text-xl"
+          >
             Case<span className="text-green-600">Beautify</span>
           </Link>
 
-          <div className="h-full flex items-center space-x-4">
+          <div className="h-full flex items-center space-x-1 lg:space-x-3">
             {user ? (
               <>
                 <Link
