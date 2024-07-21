421e5ca8c28f7915497257abc60d814a1db47751
Amplejohnny <uzunuel@gmail.com>
2024-07-21 13:10:51 +0100

    Dropzone file and upload steps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,15 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            <>{children}</>
+          </div>
+          <Footer />
+        </main>
+
+        <Toaster />
       </body>
     </html>
   );
