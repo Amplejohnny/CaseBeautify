f2819fb472bfb5ca8cb2c828b4a9fd69982d201f
Amplejohnny <uzunuel@gmail.com>
2024-08-06 11:05:41 +0100

    loginmodal and payment service

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,30 +1,30 @@
-import { type ClassValue, clsx } from 'clsx'
-import { Metadata } from 'next'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export const formatPrice = (price: number) => {
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  })
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
-  return formatter.format(price)
-}
+  return formatter.format(price);
+};
 
 export function constructMetadata({
-  title = 'CaseCobra - custom high-quality phone cases',
-  description = 'Create custom high-quality phone cases in seconds',
-  image = '/thumbnail.png',
-  icons = '/favicon.ico',
+  title = "CaseCobra - custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
 }: {
-  title?: string
-  description?: string
-  image?: string
-  icons?: string
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
 } = {}): Metadata {
   return {
     title,
@@ -35,13 +35,13 @@ export function constructMetadata({
       images: [{ url: image }],
     },
     twitter: {
-      card: 'summary_large_image',
+      card: "summary_large_image",
       title,
       description,
       images: [image],
-      creator: '@joshtriedcoding',
+      creator: "@amplejohnny",
     },
     icons,
-    metadataBase: new URL("https://casecobra.vercel.app/")
-  }
+    metadataBase: new URL("https://casebeautify.vercel.app/"),
+  };
 }
