a5743293abebd7d3bd1e0b63e59fa6396ce4e939
Amplejohnny <uzunuel@gmail.com>
2024-08-07 12:38:28 +0100

    webhooks and auth-callbacks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,13 +142,13 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p className="">
-                &quot;The case feels durable and I even got a compliment on the
+                  "The case feels durable and I even got a compliment on the
                   design. Had the case for two and a half months now and{" "}
                   <span className="p-0.5 bg-slate-800 text-white">
                     the image is super clear
                   </span>
                   , on the case I had before, the image started fading into
-                  yellow-ish color after a couple weeks. Love it.&quot;
+                  yellow-ish color after a couple weeks. Love it."
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
@@ -178,14 +178,14 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                &quot;I usually keep my phone together with my keys in my pocket
+                  "I usually keep my phone together with my keys in my pocket
                   and that led to some pretty heavy scratchmarks on all of my
                   last phone cases. This one, besides a barely noticeable
                   scratch on the corner,{" "}
                   <span className="p-0.5 bg-slate-800 text-white">
                     looks brand new after about half a year
                   </span>
-                  . I dig it.&quot;
+                  . I dig it."
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
@@ -252,7 +252,7 @@ export default function Home() {
             </li>
             <li className="w-fit">
               <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
-              Scratch- and fingerprint resistant coating
+              Scratch and fingerprint resistant coating
             </li>
             <li className="w-fit">
               <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,7 +16,7 @@ export const formatPrice = (price: number) => {
 };
 
 export function constructMetadata({
-  title = "CaseCobra - custom high-quality phone cases",
+  title = "CaseBeautify - custom high-quality phone cases",
   description = "Create custom high-quality phone cases in seconds",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
