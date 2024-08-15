67cc67394da519c71aa6a859d7e43c7a93beb825
Amplejohnny <uzunuel@gmail.com>
2024-08-15 12:01:27 +0100

    Error CORS

--- next.config.mjs
+++ next.config.mjs
@@ -13,16 +13,12 @@ const nextConfig = {
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT,HEAD",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          {
-            key: "Access-Control-Max-Age",
-            value: "1728000",
           },
         ],
       },
