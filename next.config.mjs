6f01395c6826a9badae228f4fd01ce7080cd5903
Amplejohnny <uzunuel@gmail.com>
2024-08-15 13:23:30 +0100

    Eror Sorted

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: ["/api/:path*", "/auth-callback"],
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
