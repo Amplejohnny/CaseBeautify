b105be7c91e07d3f99aff445a8a944130ba4b112
Amplejohnny <uzunuel@gmail.com>
2024-08-15 11:33:53 +0100

    Fixed CORS origin

--- next.config.mjs
+++ next.config.mjs
@@ -9,16 +9,20 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://casebeautify.vercel.app/",
+            value: "https://casebeautify.vercel.app",
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT,HEAD",
           },
           {
             key: "Access-Control-Allow-Headers",
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "Access-Control-Max-Age",
+            value: "1728000",
           },
         ],
       },
