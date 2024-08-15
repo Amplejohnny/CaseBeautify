e260f5b6704bdd86f0a7c686b05a8421c0a419af
Amplejohnny <uzunuel@gmail.com>
2024-08-15 10:31:53 +0100

    Final Cors error fixed

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://casebeautify.vercel.app/",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
