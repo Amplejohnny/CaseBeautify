66a5ec6c9f05079448888d6dd5bd023cf97a0cbc
Amplejohnny <uzunuel@gmail.com>
2024-08-15 13:06:19 +0100

    Errors fixed

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        source: ["/api/:path*", "/auth-callback"],
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
